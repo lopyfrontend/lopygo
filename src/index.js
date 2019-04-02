30fe27adf943aac0a51f4dfb78a13039ea22d54a
zy <284141050@qq.com>
2019-04-02 18:40:33 +0800

    feat(all): init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Import styles
+//import style from "./styles/main.sass";
+import style from "./scss/creative.sass";
+
+// Import scripts
+import script from "./js/creative.js";
+
+
+console.log('all files loaded');

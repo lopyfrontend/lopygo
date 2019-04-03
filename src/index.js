cd86110188919e80b92df3a7acf0b82c4caccc99
zy <284141050@qq.com>
2019-04-03 19:58:46 +0800

    fix(tmp): tmp save

--- src/index.js
+++ src/index.js
@@ -6,4 +6,13 @@ import style from "./scss/creative.sass";
 import script from "./js/creative.js";
 
 
+import img1 from './img/portfolio/fullsize/1.jpg';
+import img2 from './img/portfolio/fullsize/2.jpg';
+import img3 from './img/portfolio/fullsize/3.jpg';
+import img4 from './img/portfolio/fullsize/4.jpg';
+import img5 from './img/portfolio/fullsize/5.jpg';
+import img6 from './img/portfolio/fullsize/6.jpg';
+
+
 console.log('all files loaded');
+
